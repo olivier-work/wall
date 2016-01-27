ef1f55d3be58aa8d246ee6ccc35fc039882beb65
olivier_work <palta@work.co>
2016-01-27 02:00:47 +0000

    WIP

--- index.js
+++ index.js
@@ -4,82 +4,181 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
 var jsonMarkup = require('json-markup');
-
+var slugify = require('slugify');
 var app = express();
 
 var currentEvent = require('./events/bar.json');
+
+// paths
+const storesJsonPath = path.join(__dirname, '/stores', '/stores.json');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/welcome', function(req, res) {
+// Serve static files
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.redirect('/stores');
+});
+
+// Slides routes
+app.get('/slides/welcome', function(req, res) {
   res.render('welcome', currentEvent);
 });
-
-app.get('/moderator', function(req, res) {
+app.get('/slides/moderator', function(req, res) {
   res.render('moderator', currentEvent);
 });
-
-app.get('/thank-you', function(req, res) {
+app.get('/slides/thank-you', function(req, res) {
   res.render('thank-you');
+});
+
+app.get('/stores', function(req, res) {
+  fs.readdir(path.join(__dirname, '/stores'), function(err, stores) {
+    if (stores) {
+      res.render('stores', {
+        stores: stores
+      });
+    }
+  });
+});
+
+app.get('/store/:storeSlug', function(req, res, next) {
+  const storeSlug = req.params.storeSlug;
+
+  fs.readdir(path.join(__dirname, '/stores', storeSlug), function(err, events) {
+    events = events.map(function(event) {
+      return event.split('.')[0];
+    });
+
+    fs.readFile(storesJsonPath, function(err, storesStr) {
+      const stores = JSON.parse(storesStr);
+      res.render('store', {
+        events: events,
+        storeSlug: storeSlug,
+        store: stores[storeSlug]
+      });
+
+    });
+  });
 });
 
 app.get('/event', function(req, res) {
   fs.readdir(path.join(__dirname + '/events/'), function(err, events) {
     events = events.map(function(event) {
       return event.split('.')[0];
-    })
-
-    console.log(currentEvent)
-    console.log(jsonMarkup(currentEvent))
+    });
 
     res.render('events-list', {
       events: events,
       currentEvent: jsonMarkup(currentEvent)
     });
-  })
+  });
 });
 
-app.post('/set-current-event', function(req, res) {
-  var eventSlug = req.body.event;
-  currentEvent = require('./events/' + eventSlug);
+// app.post('/set-current-event', function(req, res) {
+//   const eventSlug = req.body.event;
+//   currentEvent = require('./events/' + eventSlug);
+//   res.redirect('/event');
+// });
 
-  res.redirect('/event');
-})
+app.get('/store/:store_slug/create-event', function(req, res) {
+  const storeSlug = req.params.store_slug;
+  const storesPath = path.join(__dirname + '/stores');
 
-app.get('/create-event', function(req, res) {
-  res.render('create-event');
+  fs.readdir(storesPath, function(err, stores) {
+    res.render('create-event', {
+      stores: stores,
+      storeSlug: storeSlug
+    });
+  });
 });
 
-app.post('/event', function(req, res) {
+app.post('/store', function(req, res) {
   var errors = [];
+  var form = req.body || {};
 
-  if(!req.body.slug) {
-    errors.push('slug is required');
+  if (!form.name) {
+    errors.push('name is required');
   }
-  if(!req.body.title) {
-    errors.push('title is required');
+
+  if (!form.ip) {
+    errors.push('ip is required');
   }
-  if(!req.body.members) {
+
+  if (errors.length > 0) {
+    return res.render('create-store', {
+      errors: errors
+    });
+  }
+
+  const storeSlug = slugify(form.name);
+  const storePath = path.join(__dirname, '/stores', storeSlug);
+
+  const event = {
+    slug: storeSlug,
+    ip: form.ip,
+    name: form.name
+  }
+
+  if (!fs.existsSync(storePath)) {
+    fs.mkdirSync(storePath);
+  }
+
+  fs.readFile(storesJsonPath, function(err, storesStr) {
+    storesStr = storesStr.toString();
+    var stores = JSON.parse(storesStr);
+    stores[storeSlug] = event;
+
+    fs.writeFile(storesJsonPath, JSON.stringify(stores, null, 2), 'utf-8', function(err) {
+      res.redirect('/store/' + storeSlug);
+    });
+  });
+});
+
+app.get('/create-store', function(req, res) {
+  res.render('create-store');
+});
+
+app.post('/store/:store_slug/event', function(req, res) {
+  var errors = [];
+  var form = req.body || {};
+  const storeSlug = req.params.store_slug;
+
+  form.storeSlug = storeSlug;
+
+  if (!form.store) {
+    errors.push('store is required');
+  } else if (!fs.existsSync(path.join(__dirname, '/stores', form.store))) {
+    errors.push('store does not exist');
+  }
+
+  if (!form.name) {
+    errors.push('name is required');
+  }
+
+  if (!form.members) {
     errors.push('need at least one member');
   } else {
-    req.body.members = req.body.members.split(',').map(function(member) {
+    form.members = form.members.split(',').map(function(member) {
       return {
         name: member.trim()
       }
     });
   }
 
-  if(errors.length > 0) {
+  if (errors.length > 0) {
     return res.render('create-event', {
       errors: errors
     });
   }
 
-  fs.writeFile(path.join(__dirname + '/events/' + req.body.slug + '.json'), JSON.stringify(req.body, 2, 2), function(err) {
-    res.redirect('/event');
+  var eventPath = path.join(__dirname, '/stores', form.store, slugify(form.name));
+
+  fs.writeFile(eventPath, JSON.stringify(form, null, 2), function(err) {
+    res.redirect(path.join('/store', storeSlug));
   });
 });
 
-app.listen(4621);
+app.listen(process.env.PORT || 4621);
